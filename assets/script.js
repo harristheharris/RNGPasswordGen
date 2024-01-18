abf4690c9515836b5b3aa54b4f1ff3a7cb72df8f
harristheharris <anthonyharris7777@gmail.com>
2024-01-17 23:36:34 -0500

    I think I'm finally done

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,107 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var trasher = document.querySelector(".trasher");
+var passLen = document.querySelector("#length")
+var caseUpper = document.querySelector("#upperCase");
+var caseLower = document.querySelector("#lowerCase");
+var caseNum = document.querySelector("#numCase");
+var caseSpecial = document.querySelector("#specialCase")
+var head1 = document.querySelector("#head1");
+
+//A function that formats a set of text
+function ohBoy() {
+  head1.setAttribute("style", "font-size: 16px; text-align: center; color: hsl(360, 91%, 36%);");
+  document.getElementById("head1").innerHTML = "Password needs to be at LEAST 8 and at MOST 128 charcters long!";
+  }
+
+//A function that formats a set of text
+function ohJeez() {
+    head1.setAttribute("style", "font-size: 16px; text-align: center; color: hsl(360, 91%, 36%);");
+    document.getElementById("head1").innerHTML = "PLEASE select one of the CASE TYPES!";
+  }
+
+function getRandom (list1){
+    var ranD = list1[Math.floor((Math.random()*list1.length))];
+    return ranD; 
+
+}
+
+function conCat(){
+  var alphabetUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  var alphabetLower = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  var nuMeric = '0123456789'.split('');
+  var specialCase = '!@#$%^&*'.split('');
+  var nul = [];
+
+    function xCase(x){
+    if (x == true){
+      var upper = alphabetUpper;
+      
+    } else {
+      var upper = nul;
+    }
+    return upper;
+  }
 
 
+    function yCase(y){
+      if (y == true){
+        var lower = alphabetLower;
+
+      } else {
+        var lower = nul;
+      }
+      return lower;
+  }
 
 
+    function zCase(z){
+    if (z == true){
+      var numm = nuMeric;
+
+    } else {
+      var numm = nul;
+    }
+      return numm;
+  }
+
+
+  function zxCase(zx){
+    if (zx == true){
+      var specCa = specialCase;
+
+    } else {
+      var specCa = nul;
+    }
+    return specCa;
+  
+}
+
+var upper = xCase(caseUpper.checked);
+var lower = yCase(caseLower.checked);
+var num = zCase(caseNum.checked);
+var specCa =zxCase(caseSpecial.checked);
+
+var passArr = lower.concat(upper, num, specCa)
+
+console.log(passArr);
+
+  return passArr;
+}
+
+function getPass(x){
+
+  var wordPass = "";
+
+  for (i = 0; i <= passLen.value-1; i++ ){
+  var coke = getRandom(x);
+  wordPass = wordPass + coke; 
+  console.log(coke);
+  }
+  
+  console.log(wordPass);
+  return wordPass;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -14,66 +112,35 @@ function writePassword() {
 
 }
 
-
-
-
 function generatePassword(){
 
-
-var alphabetLower = 'abcdefghijklmnopqrstuvwxyz'.split('');
-var alphabetUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-var nuMeric = '0123456789'.split('');
-var specialCase = '!@#$%^&*'.split('');
-
-var trasher = document.querySelector(".trasher");
-
-var passLen = document.querySelector("#length")
-var caseUpper = document.querySelector("#upperCase");
-var caseLower = document.querySelector("#lowerCase");
-var caseNum = document.querySelector("#numCase");
-var caseSpecial = document.querySelector("#specialCase")
+//values inputted by the user that we will use genertator their taylored password
 
 
-
+//A visual
   console.log(passLen.value);
   console.log(caseUpper.checked);
   console.log(caseLower.checked);
   console.log(caseNum.checked);
   console.log(caseSpecial.checked);
 
+if (passLen.value < 8 || passLen.value > 128 ){
 
+  ohBoy();
+  return;
 
+} else if (caseUpper.checked == false && caseLower.checked == false && caseNum.checked == false && caseSpecial.checked == false ) {
 
-  for (i = 0; i <= passLen.value-1; i++ ){
-    randP = getRandom(alphabetLower, alphabetUpper, nuMeric, specialCase);
-    
-  }
-  
-
-
-    
+  console.log("poop");
+  ohJeez();
+  return;
 }
 
-function getRandom (list1, list2, list3, list4){
-    var ranD = list1[Math.floor((Math.random()*list1.length))];
-    console.log(ranD)
-    return 
+var result = conCat();
+console.log(result);
+return getPass(result);
 
 }
-
-
-
-
-
-//I need a big function this function is going to take smaller functions data and use accridingly to create a password
-//Three smaller functions
-//our first function needs to determine the desired length of the password (between 8 and 128) we get this information from the user inputs.
-//The next function determines what type of cases to include
-//a Function to validate that all information matches requirments
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
